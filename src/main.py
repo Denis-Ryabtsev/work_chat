457db9a3ecd88a389e398f32b7b2cd16a1d562e3
lazarus <ryabtsev.denis05@gmail.com>
2024-12-19 21:59:02 +0700

    include auth routers

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
-from auth.router import reg_router
+from auth.router import reg_router, auth_router, test_router, logout_router
 
 app = FastAPI(
     title=f"Work_Chat"
 )
 
 app.include_router(reg_router)
+app.include_router(auth_router)
+app.include_router(test_router)
+app.include_router(logout_router)
