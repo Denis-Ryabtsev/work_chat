3dfe513020e0c58e48b62e1072185eedd5f24952
lazarus <you@example.com>
2025-01-20 16:26:58 +0700

    small fix

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -1,19 +1,22 @@
-from fastapi_users.authentication import (AuthenticationBackend,\
-                                          JWTStrategy,\
-                                          CookieTransport,
-                                          BearerTransport
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from fastapi_users.authentication import (
+                                            AuthenticationBackend,
+                                            JWTStrategy,
+                                            CookieTransport,
+                                            BearerTransport
                                         )
-
 from fastapi_users import FastAPIUsers
-from httpx_oauth.clients.github import GitHubOAuth2
 
 from config import setting
 from auth.models import LocalAccount
 from auth.manager import get_user_manager
 
+
+#   token's ttls
 LIFETIME_ACCESS=60 * 3
 LIFETIME_REFRESH=60 * 60 * 24
 
+#   token's transports
 cookie_transport = CookieTransport(
     cookie_name=f'refresh_token',
     cookie_max_age=LIFETIME_REFRESH
@@ -23,18 +26,21 @@ bearer_transport = BearerTransport(
     tokenUrl=f"auth/jwt/login"
 )
 
+#   access token's strategy
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=setting.SECRET_AUTH,
         lifetime_seconds=LIFETIME_ACCESS
     )
 
+#   refresh token's strategy
 def get_refresh_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=setting.SECRET_AUTH,
         lifetime_seconds=LIFETIME_REFRESH
     )
 
+#   token's backend
 auth_access_backend = AuthenticationBackend(
     name=f'access_jwt',
     transport=bearer_transport,
@@ -52,29 +58,7 @@ fastapi_users = FastAPIUsers[LocalAccount, int](
     auth_backends=[auth_access_backend, auth_refresh_backend]
 )
 
-#   google cloud
-
-# oauth_client = GoogleOAuth2(
-#     client_id=setting.CLIENT_ID,
-#     client_secret=setting.CLIENT_SECRET,
-#     scopes=[
-#         f"https://www.googleapis.com/auth/userinfo.profile", 
-#         f"https://www.googleapis.com/auth/userinfo.email",
-#         f"openid"
-#     ]
-#     # scopes=[
-#     #     f"openid", 
-#     #     f"email", 
-#     #     f"profile"
-#     # ]
-# )
-
-github_client = GitHubOAuth2(
-    client_id=setting.GITHUB_CLIENT_ID,
-    client_secret=setting.GITHUB_CLIENT_SECRET,
-    scopes=[
-        f"read:user",
-        f"user:email"
-    ]
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl=setting.GITHUB_AUTH_URL,
+    tokenUrl=setting.GITHUB_TOKEN_URL
 )
-
